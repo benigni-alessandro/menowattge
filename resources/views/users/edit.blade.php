0ef24a11bbbabad8967357afe8f388b0cec1e0de
alessandro benigni <alebeni02@gmail.com>
2021-10-11 21:12:12 +0200

    makeit

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <section class="section">
+        <div class="section-header">
+            <h3 class="page__heading">Edit User</h3>
+        </div>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            @if($errors->any())
+                            <div class="alert alert-dark alert-dismissible fade show" role="alert">
+                                <h3>Error on creation</h3>
+                                @foreach($errors->all() as $error)
+                                <p class="badge badge-danger">{{$error}}</p>
+                                @endforeach
+                            </div>
+                            @endif
+
+                            {!! Form::model($user, ['method'=>'PUT', 'route' => ['users.update', $user->id], 'enctype' => 'multipart/form-data']) !!}
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="name">Name</label>
+                                        {!! Form::text('name', null, array('class'=>'form-control')) !!}
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="name">Email</label>
+                                        {!! Form::text('email', null, array('class'=>'form-control')) !!}
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="photo" class="form-label"><b>Image Profile</b></label>
+                                        <input type="file" class="form-control-file @error('photo') is-invalid @enderror" id="photo" name="photo" value="{{ old('photo', $user->photo) }}">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="state" class="form-label"><b>State</b></label>
+                                        <input type="state" class="form-control-file @error('state') is-invalid @enderror" id="state" name="state" value="{{ old('state', $user->state) }}" >
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        
+                                        <label for="name">Role</label>
+                                       
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary">
+                                        Save
+                                    </button>
+                                </div>
+
+                            </div>
+                            {!! Form::close() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
