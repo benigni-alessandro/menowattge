ca8cd1127ea4eece6d1a92377d5b61d9f942edec
alessandro benigni <alebeni02@gmail.com>
2021-10-11 19:24:06 +0200

    make

--- resources/views/profile/edit_profile.blade.php
+++ resources/views/profile/edit_profile.blade.php
@@ -6,43 +6,64 @@
                 <h5 class="modal-title">Edit Profile</h5>
                 <button type="button" aria-label="Close" class="close outline-none" data-dismiss="modal">×</button>
             </div>
-            {!! Form::model(Auth::user(), ['method'=>'PUT', 'route' => ['users.update', Auth::user()->id], 'enctype' => 'multipart/form-data']) !!}
-                <div class="modal-body">
-                    {{csrf_field()}}
-                    <div class="row">
-                        <div class="form-group col-sm-6">
-                            <label for="name">Name</label>
-                            {!! Form::text('name', null, array('class'=>'form-control')) !!}
-                        </div>
-                        </div>
-                        <div class="form-group col-sm-6 d-flex">
-                       
+            <div class="section-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            @if($errors->any())
+                            <div class="alert alert-dark alert-dismissible fade show" role="alert">
+                                <h3>Error on creation</h3>
+                                @foreach($errors->all() as $error)
+                                <p class="badge badge-danger">{{$error}}</p>
+                                @endforeach
+                            </div>
+                            @endif
+
+                            {!! Form::model($user, ['method'=>'PUT', 'route' => ['users.update', $user->id], 'enctype' => 'multipart/form-data']) !!}
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="name">Name</label>
+                                        {!! Form::text('name', null, array('class'=>'form-control')) !!}
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="name">Email</label>
+                                        {!! Form::text('email', null, array('class'=>'form-control')) !!}
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
                                         <label for="photo" class="form-label"><b>Image Profile</b></label>
-                                        <input type="file" class="form-control-file @error('photo') is-invalid @enderror" id="photo" name="photo" value="{{ old('photo', Auth::user()->photo) }}">
-                             
-                                
+                                        <input type="file" class="form-control-file @error('photo') is-invalid @enderror" id="photo" name="photo" value="{{ old('photo', $user->photo) }}">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="state" class="form-label"><b>State</b></label>
+                                        <input type="state" class="form-control-file @error('state') is-invalid @enderror" id="state" name="state" value="{{ old('state', $user->state) }}" >
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="name">Role</label>
+                                        {!! Form::select('roles[]', $roles, [], array('class'=>'form-control')) !!}
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary">
+                                        Save
+                                    </button>
+                                </div>
+
+                            </div>
+                            {!! Form::close() !!}
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group col-sm-6">
-                        <label for="state" class="control-label">State</label>
-                        <input type="state" class="form-control @error('state') is-invalid @enderror" id="state" name="state" value="{{ Auth::user()->state }}">
-                        @error('state')
-                            <small class="text-danger">{{ $message }}</small>
-                        @enderror
-                        </div>
-                        <div class="form-group col-sm-6">
-                            <label>Email:</label><span class="required">*</span>
-                            <input type="text" name="email" id="email" class="form-control" required tabindex="3" disabled>
-                        </div>
-                    </div>
-                    <div class="text-right">
-                    <button type="submit" class="btn btn-primary">
-                        Save
-                     </button>
                     </div>
                 </div>
-            {!! Form::close() !!}
+            </div>
         </div>
     </div>
 </div>
