26a233c605e1c32a505851c374f35abf413d07af
alessandro benigni <alebeni02@gmail.com>
2021-10-12 16:37:31 +0200

    make

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,15 +9,13 @@
         <div class="section-body">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="card">
-                    @can('crear-post')
+                @can('crear-post')
                     <a class="btn btn-warning" href="{{route('posts.create')}}">New Post</a>
-                    @endcan
-                    </div>
+                @endcan
+                    <div class="card">
                     @foreach($posts as $post)
-                    <div class="card post">
-                        <div class="creator">
-                            <?php
+                        <div>
+                        <?php
                             $user_identity = $post->user_id;                    
                             $usuari = DB::table('users')
                                 ->where('id', '=', $user_identity)
@@ -26,30 +24,24 @@
                                 $immagine = $usuario->photo;
                             }
                             ?>
-
-                            
-                        @if(Auth::user()->photo)
+                            @if(Auth::user()->photo)
                             <img src="{{Auth::user()->photo}}"      
                             class="user_image">
                             <span><strong>{{$usuario->name}}</strong></span>
                         @endif
-
                         </div>
-                        <div class="title-post">
-                            <h3>{{ $post->title }}</h3>
-                        </div>
-                           <div class="card-body" style="max-height:500px;"> 
-                                <div class="primero">
-                
-                                    <img class="" style="width: 100%; max-width: 320px; height: auto; max-height:200px;"
-                                    src="{{$post->thumb}}" 
-                                    alt="immagine non disponibile"> 
-                                </div>
-                                <div class="segundo">
-                                   <p class="card-text">{{ $post->content }}</p>
-                                </div>                                 
-                                
+                        <div class="blog-post">
+                            <div class="blog-post-img">
+                                <img class="" 
+                                src="{{$post->thumb}}" 
+                                alt="immagine non disponibile">
                             </div>
+                            <div class="blog-post-informations">
+                                <div class="date-post">
+                                <span>ciao 121212</span>
+                            </div>
+                            <h2 class="title-post">{{ $post->title }}</h2>
+                            <p class="post-text">{{ $post->content }}</p>
                             <form action="{{ route('posts.destroy', $post->id) }}" method="POST">                                        
                                 @csrf
                                 @method('DELETE')
@@ -58,10 +50,13 @@
                                 @endcan
                             </form>  
                         </div>
+                    @endforeach 
                     </div>
-                    @endforeach
                 </div>
             </div>
         </div>
     </section>
 @endsection
+
+
+
