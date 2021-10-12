cecd8f5ac4642de9b4c11e91a86b1a7ef4af50bd
alessandro benigni <alebeni02@gmail.com>
2021-10-12 18:39:27 +0200

    make

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,6 +55,9 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'roles' => 'nullable',
+            'state'=>'required',
+            'photo'=>'image|max:6000|required',
         ]);
     }
 
