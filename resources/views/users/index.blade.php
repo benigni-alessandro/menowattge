a2afad7174eba3b71e2ee7f0536b1a1c0c8fb4bd
alessandro benigni <alebeni02@gmail.com>
2021-10-12 09:47:18 +0200

    make

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -34,7 +34,7 @@
                                         </td>
                                         <td>
                                         @can('crear-user')
-                                        <a class="btn btn-info" href="{{route('users.edit', $user->id)}}">Edit <i class="fas fa-edit"></i></a>    
+                                        <button class="btn btn-info" href="{{route('users.edit', $user->id)}}">Edit <i class="fas fa-edit"></i></button>    
                                         {!! Form::open(['method'=>'DELETE', 'route' => ['users.destroy', $user->id], 'style'=>'display:inline']) !!}
                                         <button type="submit" class="btn btn-danger">Eliminar <i class="fas fa-trash"></i></button>
                                         {!! Form::close() !!}
