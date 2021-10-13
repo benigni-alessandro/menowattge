d0ceb94bb32a2f5ad2e52cc3f30981018e14c3d0
alessandro benigni <alebeni02@gmail.com>
2021-10-13 18:57:25 +0200

    make

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -51,7 +51,6 @@ class EmailController extends Controller
             // $filePath = 'images/' . $name;
             // Storage::disk('s3')->put($filePath, file_get_contents($file));
             }
-            dd($documento['url']);
         
 
         // $data = $request->all();
